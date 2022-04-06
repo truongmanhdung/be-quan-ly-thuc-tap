dc190c82d4f59e9f0ee3e23929fe0534d052990e
Dang Trong Truong <dangtrongtruong190197@gmail.com>
2022-04-06 15:49:47 +0700

    fix: login to campus

--- src/controllers/AuthController.js
+++ src/controllers/AuthController.js
@@ -3,10 +3,12 @@ import Student from "../models/student"
 
 const { OAuth2Client } = require("google-auth-library");
 const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
+const ObjectId = require('mongodb').ObjectID;
 
 //login
 export const loginGoogle = async (req, res) => {
   const { token,cumpusId} = await req.body;
+  const cumpusObjectId = ObjectId(cumpusId)
   if (!token) {
     return res.status(401).json({ message: "Vui lòng đăng nhập tài khoản" });
   }
@@ -15,8 +17,8 @@ export const loginGoogle = async (req, res) => {
     audience: process.env.GOOGLE_CLIENT_ID,
   });
   const { email, name, picture } = ticket.getPayload();
-  const manager = await Manager.findOne({ email: email ,cumpus:cumpusId});
-  const student = await Student.findOne({ email: email ,campus_id:cumpusId})
+  const manager = await Manager.findOne({ email: email ,cumpus:cumpusObjectId});
+  const student = await Student.findOne({ email: email ,campus_id:cumpusObjectId})
   //manager and student 
   if (manager) {
     res.status(201);
@@ -26,9 +28,8 @@ export const loginGoogle = async (req, res) => {
     res.status(201);
     res.json({ student, token, name, picture,isStudent:true, message: "Đăng nhập thành công" });
   } 
-  else {
-    res.json({token:"",message:"Dang nhap that bai"});
-  }
+
+  res.json({token:"",message:"Dang nhap that bai"});
 };
 
 //logout
