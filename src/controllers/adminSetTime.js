0a33fadf901db75d01d5e077d2d81e680fefffed
haindph13158 <haindph13158@fpt.edu.vn>
2022-04-12 17:31:39 +0700

    feat:update timepicker

--- src/controllers/adminSetTime.js
+++ src/controllers/adminSetTime.js
@@ -1,18 +1,26 @@
-import ConfigTime from '../models/configTime'
+import ConfigTime from "../models/configTime";
 
-export const handleSetTimeRequest = async(req,res) => {
-    const {startTime,endTime} = req.body
-    const timeRequest = await ConfigTime.find()
-    if(timeRequest.length === 0){
-       await ConfigTime.create([{startTime,endTime}])
-       res.status(200).json({
-           message:"create time success"
-       })
-    }
-    else{
-        await ConfigTime.findOneAndUpdate({},{startTime,endTime})
-        res.status(200).json({
-            message:"update time success"
-        })
-    }
+export const handleSetTimeRequest = async (req, res) => {
+  const { startTime, endTime, typeRegister } = req.body;
+  const timeRequest = await ConfigTime.findOne({ typeRegister });
+  if (!timeRequest) {
+    const time = await ConfigTime.create(req.body);
+    res.status(200).json({
+      message: "create time success",
+      time: time,
+    });
+  } else {
+    const time = await ConfigTime.findOneAndUpdate({ typeRegister }, { startTime, endTime });
+    res.status(200).json({
+      message: "update time success",
+      time: time,
+    });
+  }
+};
+
+export const getTimeSelect = async (req, res) => {
+
 }
+
+
+
