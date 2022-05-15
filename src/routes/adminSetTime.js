9e416ff789766ffb06a3d9d48f9db64cca64fa8b
hantv15 <hantvph10714@fpt.edu.vn>
2022-05-16 01:08:57 +0700

    update/semester

--- src/routes/adminSetTime.js
+++ src/routes/adminSetTime.js
@@ -1,11 +1,25 @@
-import express from "express"
-import { getListTypeSetTime, handleSetTimeRequest, getOneTypeSetTime } from "../controllers/adminSetTime";
-import { isAuthenticateUser, authorizeRoles } from '../middlewares/CheckAuth'
+import express from "express";
+import {
+  getListTypeSetTime,
+  handleSetTimeRequest,
+  getOneTypeSetTime,
+} from "../controllers/adminSetTime";
+import { isAuthenticateUser, authorizeRoles } from "../middlewares/CheckAuth";
 
-const router = express.Router()
+const router = express.Router();
 
-router.post('/settime', isAuthenticateUser, authorizeRoles('manager'), handleSetTimeRequest)
-router.get('/settime', isAuthenticateUser, authorizeRoles('manager'), getListTypeSetTime)
-router.get('/settime/:type', isAuthenticateUser, getOneTypeSetTime)
+router.post(
+  "/settime",
+  isAuthenticateUser,
+  authorizeRoles("manager"),
+  handleSetTimeRequest
+);
+router.get(
+  "/settime",
+  isAuthenticateUser,
+  authorizeRoles("manager"),
+  getListTypeSetTime
+);
+router.get("/settime/:type", isAuthenticateUser, getOneTypeSetTime);
 
 module.exports = router;
