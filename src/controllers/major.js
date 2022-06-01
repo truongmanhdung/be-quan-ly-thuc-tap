1ee839241380f73988e9a35d2f65680aad7edde3
truongdt <truongdt@relipasoft.com>
2022-06-01 10:12:47 +0700

    feat major data

---
+++ src/controllers/major.js
@@ -0,0 +1,70 @@
+import Major from '../models/major'
+
+export const getListMajor = async(req,res) =>{
+  try {
+    const majors = await Major.find()
+    res.status(200).json({
+      majors,
+      success:true
+    })
+  } catch (error) {
+    res.json(error)
+  }
+} 
+
+export const getMajor = async(req,res) =>{
+  try {
+    const major = await Major.findById(req.params.id)
+    res.status(200).json({
+      major,
+      success:true
+    })
+  } catch (error) {
+    res.json(error)
+  }
+} 
+
+export const createMajor = async(req,res) =>{
+  try {
+    const major = await Major.create(req.body)
+    res.status(201).json({
+      major,
+      message:"Tạo ngành học thành công"
+    })
+  } catch (error) {
+    res.json({
+      error
+    })
+  }
+
+}
+
+export const updateMajor = async(req,res) =>{
+  try {
+    const major = await Major.findByIdAndUpdate(req.params.id,req.body)
+    res.status(200).json({
+      major,
+      message:"Sửa ngành học thành công"
+    })
+  } catch (error) {
+    res.json({
+      error
+    })
+  }
+
+}
+
+export const removeMajor = async(req,res) =>{
+  try {
+    const major = await Major.findByIdAndRemove(req.params.id)
+    res.status(200).json({
+      message:"Xóa ngành học thành công"
+    })
+  } catch (error) {
+    res.json({
+      error
+    })
+  }
+
+}
+
---
+++ src/routes/major.js
@@ -0,0 +1,14 @@
+import express from "express";
+import { createMajor, getListMajor, removeMajor, updateMajor } from "../controllers/major";
+import { authorizeRoles, isAuthenticateUser } from "../middlewares/CheckAuth";
+
+const router = express.Router();
+
+router.get("/major",isAuthenticateUser, getListMajor);
+router.get("/major/:id",isAuthenticateUser, getListMajor);
+router.post("/major",isAuthenticateUser,authorizeRoles("manager"), createMajor);
+router.patch("/major/:id",isAuthenticateUser,authorizeRoles("manager"), updateMajor);
+router.delete("/major/:id",isAuthenticateUser,authorizeRoles("manager"), removeMajor);
+
+
+module.exports = router;
