d1d6f36a8cd2f52b2646243c1b22a399fc606856
Dương  Điệp <zenkyxz@gmail.com>
2022-09-22 17:53:29 +0700

    update;

--- src/controllers/requestStudentController.js
+++ src/controllers/requestStudentController.js
@@ -1,25 +1,44 @@
-import requestStudentToManagerModel from '../models/requestStudentToManagerModel';
-import Student from '../models/student';
-import { defaultCvStudent, defaultForm, defaultReport } from '../utils/defaultValueStudent';
+import requestStudentToManagerModel from "../models/requestStudentToManagerModel";
+import Student from "../models/student";
+import {
+  defaultCvStudent,
+  defaultForm,
+  defaultReport,
+} from "../utils/defaultValueStudent";
 export function getListRequestStudent(req, res) {}
 
 export async function sendRequestToManager(req, res) {
+  const findRequest = await requestStudentToManagerModel.findOne({
+    userId: req.body.userId,
+  });
+
   try {
-    await requestStudentToManagerModel.create(req.body);
-    res.status(201).json({
-      success: true,
-    });
+    if (findRequest && findRequest.type === req.body.type) {
+      return res.status(400).json({
+        success: false,
+        message: "Yêu cầu đã tồn tại",
+      });
+    } else {
+      await requestStudentToManagerModel.create(req.body);
+      res.status(201).json({
+        success: true,
+        message: "Thành công",
+      });
+    }
   } catch (error) {
     res.status(400).json({
       success: false,
       error,
+      message: "Có lỗi xảy ra",
     });
   }
 }
 
 export async function getRequestOfStudent(req, res) {
   try {
-    const data = await requestStudentToManagerModel.find(req.query).populate('userId');
+    const data = await requestStudentToManagerModel
+      .find(req.query)
+      .populate("userId");
     res.status(200).json({
       success: true,
       data: data,
@@ -34,44 +53,46 @@ export async function getRequestOfStudent(req, res) {
 
 export async function resetStudent(req, res) {
   const { type, id, useId } = req.body;
-    let valueReset = {};
-    if (type === process.env.cv) {
-      valueReset = defaultCvStudent;
-    }
-    if (type === process.env.form) {
-      valueReset = defaultForm;
-    }
-    if (type === process.env.report) {
-      valueReset = defaultReport;
-    }
-
+  let valueReset = {};
+  if (type === process.env.cv) {
+    valueReset = defaultCvStudent;
+  }
+  if (type === process.env.form) {
+    valueReset = defaultForm;
+  }
+  if (type === process.env.report) {
+    valueReset = defaultReport;
+  }
 
   try {
-    Student.findOneAndUpdate({_id: req.params.id}, valueReset, { new: true }).then((r) => requestStudentToManagerModel.findByIdAndDelete(id)).then(r => res.status(200).json({
-      success:true,
-      message: 'Thành công'
-    }));
+    Student.findOneAndUpdate({ _id: req.params.id }, valueReset, { new: true })
+      .then((r) => requestStudentToManagerModel.findByIdAndDelete(id))
+      .then((r) =>
+        res.status(200).json({
+          success: true,
+          message: "Thành công",
+        })
+      );
   } catch (error) {
     res.status({
       success: false,
-      message: 'Có lỗi sảy ra',
+      message: "Có lỗi sảy ra",
     });
   }
 }
 
-export async function cancelResetStudent(req,res){
+export async function cancelResetStudent(req, res) {
   try {
-    await requestStudentToManagerModel.findByIdAndDelete(req.params.id) 
-    
-    res.status(204).json({
+    await requestStudentToManagerModel.findByIdAndDelete(req.params.id);
+
+    res.status(200).json({
       success: true,
-      message: 'Thành công'
-    })
+      message: "Thành công",
+    });
   } catch (error) {
-  res.status(400).json({
-    success: false,
-    message: 'Thất bại'
-  })
+    res.status(400).json({
+      success: false,
+      message: "Thất bại",
+    });
   }
-    
 }
